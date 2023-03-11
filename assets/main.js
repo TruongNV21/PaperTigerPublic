a43a179e343d6e66cd547b6b32cb10a847f033da
Nguyen Van Truong <ttntruong79@gamil.com>
2023-03-11 16:58:20 +0700

    update2footer

--- assets/main.js
+++ assets/main.js
@@ -3,17 +3,19 @@ const spanServices = document.querySelector('.header-nav-mobile__link.have-dropd
 const headerNavMobileItemHaveDropdown = document.querySelector('.header-nav-mobile__item-have-dropdown');
 const headerMenuButton = document.querySelector('.header__menu-mobile');
 const mobileNavList = document.querySelector('.header-nav-mobile__list')
-
+const body = document.querySelector('body')
 
 spanServices.addEventListener('click', (e) =>{
     e.stopPropagation();
-    headerNavMobileItemHaveDropdown.classList.toggle('close-dropdown')
+    headerNavMobileItemHaveDropdown.classList.toggle('close-dropdown');
+    body.classList.toggle('navbar-on-scroll')
 })
 
 
 headerMenuButton.addEventListener('click', (e)=>{
     e.stopPropagation();
     mobileNavList.classList.toggle('hidden');
+    body.classList.toggle('navbar-on-scroll')
     headerMenuButton.classList.toggle('close-nav-mobile-list')
 })
 
